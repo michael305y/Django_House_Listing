ddee77d86d24a2501d6be7b3bc77408f680717c3
Michael305y <antolando231@gmail.com>
2023-06-13 20:25:02 +0300

    updating env file

--- real_estate/real_estate/settings.py
+++ real_estate/real_estate/settings.py
@@ -12,15 +12,27 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import os
+from dotenv import load_dotenv
+
+
+ENV_FILE_PATH = os.path.join(BASE_DIR, '../../.real_estate/CONFIG_DIR/.env')
+
+# Load the .env file
+load_dotenv(ENV_FILE_PATH)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cqnbb!!i%ihs%wm6ji#r$$sus0hfx1+*yu*jqb70xe(03gp))p'
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-cqnbb!!i%ihs%wm6ji#r$$sus0hfx1+*yu*jqb70xe(03gp))p'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
